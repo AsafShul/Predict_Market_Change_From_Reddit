0ecc0407db6dfff2eeb0468a2142268a39834040
asafshul <asafshul96@gmail.com>
2023-06-02 01:17:28 +0300

    fine tune

--- src/DatabaseFormatter.py
+++ src/DatabaseFormatter.py
@@ -1,5 +1,7 @@
 import os
 import json
+import time
+
 import numpy as np
 import pandas as pd
 from tqdm import tqdm
@@ -8,25 +10,35 @@ import datetime as dt
 BASE_DATA_DIR = '../data'
 STOCKS_DIR = os.path.join(BASE_DATA_DIR, 'reddit', 'stocks')
 BETS_DIR = os.path.join(BASE_DATA_DIR, 'reddit', 'wallStreetBets')
+WORLD_DIR = os.path.join(BASE_DATA_DIR, 'reddit', 'world_news')
 
 SUBMISSION_COLS = ['created_utc', 'id', 'num_comments', 'title', 'selftext', 'score']
 
 class DatabaseFormatter:
     @staticmethod
+    # def format(stock_data_path_, bets_data_path_, world_data_path_, labels_path_, output_path_=None):
     def format(stock_data_path_, bets_data_path_, labels_path_, output_path_=None):
-        bets_df = pd.read_csv(bets_data_path_, index_col='date_index', parse_dates=True)
-        stocks_df = pd.read_csv(stock_data_path_, index_col='date_index', parse_dates=True)
+        bets_df = pd.read_csv(bets_data_path_, index_col='post_time', parse_dates=True)
+        stocks_df = pd.read_csv(stock_data_path_, index_col='post_time', parse_dates=True)
+        # world_df = pd.read_csv(world_data_path_, index_col='post_time', parse_dates=True)
+
         labels = DatabaseFormatter._label_data(labels_path_)
 
-        df_ = pd.concat([bets_df, stocks_df], axis=0)
-        df_ = df_[(df_.num_comments < 1000) & (df_.num_comments > 5)]
+        # df_ = pd.concat([bets_df, stocks_df, world_df], axis=0).sort_index()
+        df_ = pd.concat([bets_df, stocks_df], axis=0).sort_index()
+        df_.selftext = df_.selftext.fillna(' ')
+        df_.title = df_.title.fillna(' ')
+
+        df_['post'] = df_.title + ' $$$ ' + df_.selftext
+
+        df_ = df_[(df_.num_comments < 1500) & (df_.num_comments > 5)]
         df_ = df_[~df_.selftext.str.contains('www.reddit.com/poll')]  # polls
         df_ = df_[~df_.selftext.str.startswith('&amp')]  # probably a memes
 
         df_ = df_[[d in labels.index for d in df_.index.date]]  # remove non-trading days
         df_['label'] = labels[df_.index.date].values
 
-        df_.drop(columns=['id', 'num_comments'], inplace=True)
+        df_.drop(columns=['id', 'num_comments', 'title', 'num_comments', 'selftext', 'score'], inplace=True)
 
         if output_path_ is not None:
             df_.reset_index().to_feather(output_path_, compression='zstd')
@@ -44,11 +56,12 @@ class DatabaseFormatter:
 
         df = df.set_index('date')
         percent_change = (1 - df.close / df.open) * 100
-        labels = ((percent_change.abs() > eps) * np.sign(percent_change)).astype(int).shift(-1).dropna()
+        labels = ((percent_change.abs() > eps) * np.sign(percent_change)).astype(int).shift(-1).dropna() + 1
         return labels
 
     @staticmethod
     def format_raw_submission_data(dir_path, filename):
+        time.sleep(1)
         print(f'Formatting raw {filename}...')
 
         lines = []
@@ -60,24 +73,24 @@ class DatabaseFormatter:
                     post_datetime = dt.datetime.fromtimestamp(int(line['created_utc']))
                     if post_datetime.date().year < 2018:
                         continue
-                    line['date_index'] = post_datetime
+                    line['post_time'] = post_datetime
                     line.pop('created_utc')
                 else:
                     continue
                 if 'selftext' in line.keys():
-                    if line['selftext'] == '[removed]' or line['selftext'] == '[deleted]' or line['selftext'] == '':
+                    if line['selftext'] == '[removed]' or line['selftext'] == '[deleted]':
                         continue
                 else:
                     continue
 
                 # drop keys that are not in THE_BEST
                 for key in list(line.keys()):
-                    if key not in SUBMISSION_COLS + ['date_index']:
+                    if key not in SUBMISSION_COLS + ['post_time']:
                         line.pop(key)
 
                 lines.append(line)
 
-            df = pd.DataFrame(lines).set_index('date_index')
+            df = pd.DataFrame(lines).set_index('post_time')
             df.to_csv(os.path.join(dir_path, f'{filename}.csv'))
             print()
 
@@ -87,13 +100,18 @@ if __name__ == '__main__':
     # format:
     # DatabaseFormatter.format_raw_submission_data(STOCKS_DIR, 'stocks_submissions')
     # DatabaseFormatter.format_raw_submission_data(BETS_DIR, 'wallstreetbets_submissions')
+    # DatabaseFormatter.format_raw_submission_data(WORLD_DIR, 'worldnews_submissions')
 
     bets_path = '../data/reddit/wallStreetBets/wallstreetbets_submissions.csv'
     stocks_path = '../data/reddit/stocks/stocks_submissions.csv'
+    # world_path = '../data/reddit/world_news/worldnews_submissions.csv'
+
     labels_path = '../data/spy/spy.csv'
+
     output_path = '../data/formatted_df.ftr.zstd'
 
     df = DatabaseFormatter.format(stock_data_path_=stocks_path, bets_data_path_=bets_path,
+                                  # world_data_path_=world_path, labels_path_=labels_path, output_path_=output_path)
                                   labels_path_=labels_path, output_path_=output_path)
 
     print()
---
+++ src/finetune.py
@@ -0,0 +1,146 @@
+import os
+import wandb
+import numpy as np
+import pandas as pd
+import datetime as dt
+
+from tqdm import tqdm
+from evaluate import load
+from sklearn.metrics import f1_score
+
+from get_dataset import Datasets
+
+import datasets
+
+from transformers import (
+    AutoConfig,
+    AutoModelForSequenceClassification,
+    AutoTokenizer,
+    Trainer,
+    EvalPrediction,
+    TrainingArguments
+)
+
+ROBERTA_MAX_LENGTH = 512
+
+MODEL = f"cardiffnlp/twitter-roberta-base-sentiment-latest"
+
+
+class RedditStockPredictionBaseline:
+    def __init__(self):
+        self.model = AutoModelForSequenceClassification.from_pretrained(MODEL, cache_dir="./cache")
+        self.tokenizer = AutoTokenizer.from_pretrained(MODEL)
+        self.config = AutoConfig.from_pretrained(MODEL)
+        self.datasets = Datasets()
+        self.pre_process_func = self.get_tokenizer_func(self.tokenizer)
+        self.training_args = TrainingArguments(output_dir="results",
+                                               evaluation_strategy="epoch",
+                                               save_strategy="epoch")
+
+    @staticmethod
+    def preprocess(text):
+        text = text.replace('\n', ' ')
+        text = text.replace('\r', ' ')
+        text = text.replace('\t', ' ')
+        text = ' '.join(('http' if w.startswith('http') else w) for w in text.split(' '))
+        return text
+
+    @staticmethod
+    def get_metric_func(metric_name):
+        metric = load(metric_name)
+
+        def compute_metrics(p: EvalPrediction):
+            preds = np.argmax(p.predictions[0] if isinstance(p.predictions, tuple) else p.predictions, axis=1)
+            result = metric.compute(predictions=preds, references=p.label_ids)[metric_name]
+            return {metric_name: result}
+
+        return compute_metrics
+
+    def get_tokenizer_func(self, tokenizer):
+        def preprocess_function(examples):
+            result = tokenizer(examples['text'], truncation=True, padding=True,
+                               max_length=ROBERTA_MAX_LENGTH, return_tensors='pt')
+            return result
+
+        return preprocess_function
+
+    def train(self):
+        wandb.init(project="reddit-stock-prediction",
+                   name=f"reddit-stock-prediction_{dt.datetime.now()}")
+        train_set = self.datasets.train_set.rename(columns={'post': 'text'})
+        val_set = self.datasets.val_set.rename(columns={'post': 'text'})
+
+        train_set.text = train_set.text.apply(self.preprocess)
+        val_set.text = val_set.text.apply(self.preprocess)
+
+        train_set.label = train_set.label.astype(int)
+        val_set.label = val_set.label.astype(int)
+
+        train_set = datasets.Dataset.from_pandas(train_set)
+        val_set = datasets.Dataset.from_pandas(val_set)
+
+        train_set = train_set.map(self.pre_process_func, batched=True, batch_size=None)
+        val_set = val_set.map(self.pre_process_func, batched=True, batch_size=None)
+
+        trainer = Trainer(
+            model=self.model,
+            args=self.training_args,
+            train_dataset=train_set,
+            eval_dataset=val_set,
+            compute_metrics=self.get_metric_func('accuracy'),
+            tokenizer=self.tokenizer,
+        )
+
+
+        train_result = trainer.train()
+        trainer.save_model(f'models/{MODEL}')
+        test_score = self.test()
+        wandb.finish()
+
+        return train_result, test_score
+
+    def test(self):
+        test_set = self.datasets.test_set.rename(columns={'post': 'text'})
+        test_set = test_set.set_index(pd.to_datetime(test_set.post_time))
+        test_set.text = test_set.text.apply(self.preprocess)
+        test_set.label = test_set.label.astype(int)
+
+        by_day = test_set.groupby(test_set.index.date)
+        results = pd.DataFrame(columns=['true', 'predicted'])
+
+        for day, df in tqdm(by_day, total=len(by_day), desc="Testing day by day"):
+            predictions = np.empty((0, 3))
+
+            labels = df.label.unique()
+            assert len(labels) == 1
+            label = labels[0]
+
+            for i, row in df.iterrows():
+                encoded_input = self.tokenizer(row.text, return_tensors='pt',
+                                               truncation=True, padding=True, max_length=ROBERTA_MAX_LENGTH)
+                prediction = self.model(**encoded_input)
+                predictions = np.vstack((predictions, prediction.logits.detach().numpy()))
+
+            scores = predictions.mean(axis=0)
+            # scores = softmax(scores)
+            day_prediction = scores.argmax()
+
+            results.loc[day] = [label, day_prediction]
+
+        score = f1_score(results.true, results.predicted, average='macro')
+        return score
+
+
+def main():
+    wandb.login(key=os.environ["WANDB_API_KEY"])
+    r = RedditStockPredictionBaseline()
+    train_results, test_score = r.train()
+    print('train_results:')
+    print(train_results)
+
+    test_score = r.test()
+    print("F1 Score:", test_score)
+
+
+if __name__ == "__main__":
+    main()
